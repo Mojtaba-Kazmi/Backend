6c9bf6112473be5c9b628bbc93ea005e4192b915
Mojtaba KAZMI <88171449+Mojtaba-Kazmi@users.noreply.github.com>
2022-01-18 03:47:14 +0500

    Update sauce.js

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,13 +1,5 @@
 const express = require("express");
-<<<<<<< HEAD
-const router = express.Router();
-const multer = require("../middleware/multer-config");
-const auth = require("../middleware/auth");
-const checkSauceInput = require("../middleware/check-sauce-input")
 
-const sauceCtrl = require("../controllers/sauce");
-
-=======
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 const checkSauceInput = require("../middleware/check-sauce-input")
@@ -15,7 +7,6 @@ const sauceCtrl = require("../controllers/sauce");
 
 const router = express.Router();
 
->>>>>>> c3b01109f8d86ea8e404f22098859dee29f0e1a7
 router.get("/", auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post("/", auth, multer, checkSauceInput, sauceCtrl.createSauce);
