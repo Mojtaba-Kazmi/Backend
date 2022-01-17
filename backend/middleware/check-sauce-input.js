ba8d0b96e31d798695248f7a0be4d88dd08c046e
Mojtaba KAZMI <88171449+Mojtaba-Kazmi@users.noreply.github.com>
2022-01-18 03:33:38 +0500

    Update check-sauce-input.js

--- backend/middleware/check-sauce-input.js
+++ backend/middleware/check-sauce-input.js
@@ -1,9 +1,7 @@
 module.exports = (req, res, next) => {
-<<<<<<< HEAD
+  
   // If this is the post route
-=======
-  // Si il s'agit de la route post
->>>>>>> c3b01109f8d86ea8e404f22098859dee29f0e1a7
+
   if (JSON.parse(req.body.sauce !== undefined)) {
     const sauce = JSON.parse(req.body.sauce);
     let { name, manufacturer, description, mainPepper } = sauce;
@@ -14,11 +12,8 @@ module.exports = (req, res, next) => {
     }
     toTrim(name, manufacturer, description, mainPepper);
 
-<<<<<<< HEAD
-   // Check the number of characters after trim()
-=======
-    // Vérification du nombre de caractères après avoir trim()
->>>>>>> c3b01109f8d86ea8e404f22098859dee29f0e1a7
+   //Check the number of characters after trim()
+
     const hasThreeCharacters = (currentValue) => currentValue.length >= 3;
     if (trimedTab.every(hasThreeCharacters)) {
       next();
@@ -26,12 +21,8 @@ module.exports = (req, res, next) => {
       throw new Error("Tous les champs doivent faire au moins 3 caractères");
     }
   } else {
-<<<<<<< HEAD
     
   // If this is the Put route
-=======
-    // Si il s'agit de la route put
->>>>>>> c3b01109f8d86ea8e404f22098859dee29f0e1a7
     const sauce = req.body;
     let { name, manufacturer, description, mainPepper } = sauce;
     let trimedTab = [];
@@ -40,13 +31,9 @@ module.exports = (req, res, next) => {
       trimedTab = string.map((elt) => elt.trim());
     }
     toTrim(name, manufacturer, description, mainPepper);
-
-<<<<<<< HEAD
     
-    // Check the number of characters after trim()
-=======
-    // Vérification du nombre de caractères après avoir trim()
->>>>>>> c3b01109f8d86ea8e404f22098859dee29f0e1a7
+    //Check the number of characters after trim()
+
     const hasThreeCharacters = (currentValue) => currentValue.length >= 3;
     if (trimedTab.every(hasThreeCharacters)) {
       next();
